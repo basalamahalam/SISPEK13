8a1c56ef5e2b7cb8575a7b493923fa4d6548ba5e
basalamahalam <basalamahalam@upi.edu>
2024-07-21 19:30:27 +0700

    fix sejarah

--- resources/views/about/sejarah.blade.php
+++ resources/views/about/sejarah.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.main')
 
 @section('container')
-<section id="sejarah" class="pt-36 pb-32 bg-slate-600">
-    <div class="container">
-        <div class="mx-auto text-center mb-10">
+<section id="sejarah" class="pb-32 pt-36 bg-slate-600">
+    <div class="px-4 md:px-12 lg:px-20 2xl:px-44">
+        <div class="mx-auto mb-10 text-center">
             <h4 class="font-semibold text-lg text-primary mb-2 tracking-[0.3rem]">SISPEK13</h4>
-            <h2 class="font-bold text-white text-2xl mb-4 lg:text-4xl">Sejarah <span class="font-semibold text-dark">Website</span></h2>
+            <h2 class="mb-4 text-2xl font-bold text-white lg:text-4xl">Sejarah <span class="font-semibold text-dark">Website</span></h2>
         </div>
-        <img src="../img/assets/bg-web.png" alt="sispek13" class="w-full rounded-xl mb-10">
-        <div class="p-4 bg-slate-800 rounded-lg shadow-lg shadow-dark">
-            <h2 class="text-2xl font-semibold text-white mb-5">Tentang Website</h2>
+        <img src="../img/assets/bg-web.png" alt="sispek13" class="w-full mb-10 rounded-xl">
+        <div class="p-4 rounded-lg shadow-lg bg-slate-800 shadow-dark">
+            <h2 class="mb-5 text-xl font-semibold text-white lg:text-2xl">Tentang Website</h2>
             <article class="text-justify text-slate-200">
-                <p>Halo, Thirteenagers!!!</p>
+                <p class="text-sm lg:text-base">Halo, Thirteenagers!!!</p>
                 
-                <p>Kenalin nih, kita dari Sixth Projects, sebuah tim yang beranggotakan enam orang diantaranya ada Alam, Fachri, Raisyad, Ija, Wildan, dan Ade. Cerita kami dimulai dari bangku kuliah, di mana kami semua terjun dalam misi superhero untuk menaklukkan tugas mata kuliah yang cukup bikin pusing tujuh keliling.
+                <p class="text-sm lg:text-base">Kenalin nih, kita dari Sixth Projects, sebuah tim yang beranggotakan enam orang diantaranya ada Alam, Fachri, Raisyad, Ija, Wildan, dan Ade. Cerita kami dimulai dari bangku kuliah, di mana kami semua terjun dalam misi superhero untuk menaklukkan tugas mata kuliah yang cukup bikin pusing tujuh keliling.
                     Awalnya, ide bikin website ini cuma sebagai senjata utama buat menghadapi tugas tersebut. Tapi, seperti layaknya plot twist dalam film-film, apa yang dimulai sebagai proyek kelas berubah jadi sesuatu yang lebih seru. Kami semua bahu-membahu, dari merancang, coding, sampai ngopi bareng (yang terakhir ini penting banget buat menjaga stamina dan mood, hehe).</p>
                 <br>
 
-                <p>Kemudian, setelah tugas selesai dan nilai sudah di tangan, Alam, salah satu dari kita, merasa ini belum ending yang keren. Dia punya visi buat terus mengembangkan website ini lebih lanjut. Jadi, dengan semangat 45 dan kopi di samping laptop, Alam meneruskan estafet ini sampai akhirnya, tepat pada 13 Januari 2024, versi terbaru website kita bisa online.
+                <p class="text-sm lg:text-base">Kemudian, setelah tugas selesai dan nilai sudah di tangan, Alam, salah satu dari kita, merasa ini belum ending yang keren. Dia punya visi buat terus mengembangkan website ini lebih lanjut. Jadi, dengan semangat 45 dan kopi di samping laptop, Alam meneruskan estafet ini sampai akhirnya, tepat pada 13 Januari 2024, versi terbaru website kita bisa online.
                     Website ini jadi bukti bahwa dari tugas kuliah yang bikin garuk-garuk kepala, kita bisa buat sesuatu yang keren dan berguna. Kami harap kalian, terutama kalian yang masih SMK, bisa terinspirasi. Bukan cuma soal coding atau desain web, tapi tentang kerja sama, kegigihan, dan berani meneruskan sesuatu yang kalian percayai.
                     Yuk, selami lebih dalam website kita. Siapa tau bisa jadi bahan obrolan seru, inspirasi, atau malah muncul ide gila kalian sendiri.</p>
 
