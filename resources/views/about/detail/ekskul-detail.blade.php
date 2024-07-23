09283251cf0af1c69bc34af2ab154f94a78f2a17
basalamahalam <basalamahalam@upi.edu>
2024-07-23 14:54:34 +0700

    fix jurnal responsive

--- resources/views/about/detail/ekskul-detail.blade.php
+++ resources/views/about/detail/ekskul-detail.blade.php
@@ -14,7 +14,7 @@
                     {!! $data->body !!}
                 </article>
                 <div class="flex justify-between">
-                    <a href="/ekskul" class="flex items-center text-sm font-medium text-white transition-all duration-300 ease-in-out lg:font-semibold lg:text-lg hover:text-primary">
+                    <a href="/ekskul" class="flex items-center text-sm font-medium text-yellow-500 transition-all duration-300 ease-in-out lg:font-semibold lg:text-lg hover:text-primary">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-3 bi bi-arrow-left-short" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
                         </svg>
--- resources/views/media/jurnalosis.blade.php
+++ resources/views/media/jurnalosis.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.main')
 
 @section('container')
-<section id="jurnal" class="pt-36 pb-32 bg-slate-600">
-    <div class="container">
-        <div class="mx-auto text-center mb-10">
+<section id="jurnal" class="pb-32 pt-36 bg-slate-600">
+    <div class="px-4 md:px-12 2xl:px-48 animate-fade-up">
+        <div class="mx-auto mb-10 text-center">
             <h4 class="font-semibold text-lg text-primary mb-2 tracking-[0.3rem]">{{ $title }}</h4>
-            <h2 class="font-bold text-white text-2xl mb-4 sm:text-xl lg:text-4xl">{{ $subtitle }}</h2>
+            <h2 class="mb-4 text-2xl font-bold text-white sm:text-xl lg:text-4xl">{{ $subtitle }}</h2>
         </div>
         <div class="mb-10">
-            <form action="/{{ $name }}" class="w-full flex items-center justify-center">
+            <form action="/{{ $name }}" class="flex items-center justify-center w-full">
                 <input
                     class="w-[80%] lg:w-1/2 py-2 pl-3 rounded-l-lg border-2 border-sky-700 focus:outline-none "
                     type="text"
@@ -21,28 +21,31 @@
         </div>
 
         @if($data->count())
-            <div class="flex flex-wrap mb-5 ">
-                @foreach($data as $post)
-                <div class="w-full px-4 lg:w-1/2 xl:w-1/3">
-                    <div class="bg-slate-800 rounded-xl shadow-lg overflow-hidden mb-10">
-                        <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->image }}" class="h-[220px]">
-                        <div class="py-8 px-6">
-                            <h3 class="block  font-semibold text-xl text-white truncate">{{ $post->title }}</h3>
-                            <p class="font-small text-sm text-slate-400 mb-3">
-                                <small>
-                                    By {{ $post->author }} {{ $post->created_at->diffForHumans() }}
-                                </small>
-                            </p>
-                            <p class="font-medium text-base text-slate-300 mb-6 truncate">{{ $post->excerpt }}</p>
-                            <a href="/{{ $name }}/{{ $post->slug }}" class="font-medium text-sm text-white bg-primary py-2 px-4 rounded-lg hover:opacity-80">Baca Selengkapnya</a>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
+           <div class="grid grid-cols-1 gap-6 px-4 mb-5 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 lg:px-0">
+    @foreach($data as $post)
+    <div class="px-2 md:px-4">
+        <div class="mb-10 overflow-hidden shadow-lg bg-slate-700 rounded-xl">
+            <div class="h-64">
+                <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->image }}" class="object-cover w-full h-full">
             </div>
+            <div class="px-6 py-8">
+                <h3 class="block text-xl font-semibold text-white truncate">{{ $post->title }}</h3>
+                <p class="mb-3 text-sm font-small text-slate-400">
+                    <small>
+                        By {{ $post->author }} {{ $post->created_at->diffForHumans() }}
+                    </small>
+                </p>
+                <p class="mb-6 text-base font-medium truncate text-slate-300">{{ $post->excerpt }}</p>
+                <a href="/{{ $name }}/{{ $post->slug }}" class="px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary hover:opacity-80">Baca Selengkapnya</a>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div>
+
         @else
         <div class="container">
-            <p class="font-semibold text-2xl text-white text-center">Tidak ada {{ $name }} yang ditemukan.</p>
+            <p class="text-2xl font-semibold text-center text-white">Tidak ada {{ $name }} yang ditemukan.</p>
         </div>
         @endif
 
