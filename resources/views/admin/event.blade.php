3707c28248420f99a5dd0d1a09d7def37ff5ea22
basalamahalam <basalamahalam@upi.edu>
2024-07-26 18:38:42 +0700

    fix session media

--- resources/views/admin/event.blade.php
+++ resources/views/admin/event.blade.php
@@ -3,51 +3,60 @@
 @section('container')
 <section class="ml-0 lg:ml-[250px] p-8">
     @if(session()->has('success'))
-        <div class="w-1/2 flex justify-between mx-auto bg-green-100 border-l-4 border-green-500 text-green-700 p-4 relative" role="alert">
+        <div id="success-message" class="relative flex justify-between w-1/2 p-4 mx-auto text-green-700 bg-green-100 border-l-4 border-green-500" role="alert">
             {{ session('success') }}
             <button
-                class=" text-green-600 hover:text-green-400 font-bold text-medium"
+                class="font-bold text-green-600 hover:text-green-400 text-medium"
                 onclick="this.parentElement.style.display='none'"
             >
                 X
             </button>
         </div>
+
+        <script>
+            setTimeout(() => {
+                var successMessage = document.getElementById("success-message");
+                if(successMessage){
+                    successMessage.style.display = 'none';
+                }
+            }, 3000);
+        </script>
     @endif
-    <a href="/profil-osis" class="flex items-center font-semibold text-lg text-primary mb-10 hover:text-sky-800 transition-all duration-300 ease-in-out">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-short mr-3" viewBox="0 0 16 16">
+    <a href="/profil-osis" class="flex items-center mb-10 text-lg font-semibold transition-all duration-300 ease-in-out text-primary hover:text-sky-800">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mr-3 bi bi-arrow-left-short" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
         </svg>
         Kembali
     </a>
-    <h2 class="text-dark font-bold text-4xl mb-5">{{ $title }}</h2>
-    <h2 class="text-sky-800 font-bold text-2xl mb-10">{{ $subtitle }}</h2>
-    <a href="/event/{{ $slug }}/create" class="text-sm font-semibold text-white bg-primary py-3 px-6 rounded-md hover:shadow-lg hover:opacity-50 transition duration-300 ease-in-out">+ Tambah Acara</a>
-    <div class="mt-10 rounded-lg shadow-lg bg-white flex flex-col overflow-x-auto mb-10">
+    <h2 class="mb-5 text-4xl font-bold text-dark">{{ $title }}</h2>
+    <h2 class="mb-10 text-2xl font-bold text-sky-800">{{ $subtitle }}</h2>
+    <a href="/event/{{ $slug }}/create" class="px-6 py-3 text-sm font-semibold text-white transition duration-300 ease-in-out rounded-md bg-primary hover:shadow-lg hover:opacity-50">+ Tambah Acara</a>
+    <div class="flex flex-col mt-10 mb-10 overflow-x-auto bg-white rounded-lg shadow-lg">
         <div class="inline-block min-w-full overflow-hidden">
             @if($data->count())
             <table class="min-w-full mb-0">
-                <thead class="border-b rounded-t-lg text-left">
+                <thead class="text-left border-b rounded-t-lg">
                 <tr>
-                    <th scope="col" class="rounded-tl-lg text-sm font-medium px-4 py-2">No</th>
-                    <th scope="col" class="text-sm font-medium px-4 py-2">Nama Acara</th>
-                    <th scope="col" class="text-sm font-medium px-4 py-2">Deskripsi Acara</th>
-                    <th scope="col" class="text-sm font-medium px-4 py-2">Tanggal Acara</th>
-                    <th scope="col" class="rounded-tr-lg text-sm font-medium px-4 py-2"></th>
+                    <th scope="col" class="px-4 py-2 text-sm font-medium rounded-tl-lg">No</th>
+                    <th scope="col" class="px-4 py-2 text-sm font-medium">Nama Acara</th>
+                    <th scope="col" class="px-4 py-2 text-sm font-medium">Deskripsi Acara</th>
+                    <th scope="col" class="px-4 py-2 text-sm font-medium">Tanggal Acara</th>
+                    <th scope="col" class="px-4 py-2 text-sm font-medium rounded-tr-lg"></th>
                 </tr>
                 </thead>
                 <tbody>
                     @foreach ($data as $event)     
-                    <tr class="border-b bg-gray-100">
-                        <th class="text-sm font-medium px-4 py-2 whitespace-nowrap text-left" scope="row">{{ $number }}</th>
-                        <td class="text-sm font-normal px-4 py-2 whitespace-nowrap text-left text-gray-500">{{ $event->nama_acara }}</td>
-                        <td class="text-sm font-normal px-4 py-2 whitespace-nowrap text-left text-gray-500">{!! Str::limit($event->deskripsi_acara, $limit = 40, $end = '...') !!}</td>
-                        <td class="text-sm font-normal px-4 py-2 whitespace-nowrap text-left text-gray-500">{{ $event->tanggal_acara }}</td>
-                        <td class="text-sm font-normal px-4 py-2 whitespace-nowrap text-right">
-                            <a href="/event/{{ $slug }}/{{ $event->id }}/edit" class="font-medium text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 transition duration-300 ease-in-out">Edit</a>
+                    <tr class="bg-gray-100 border-b">
+                        <th class="px-4 py-2 text-sm font-medium text-left whitespace-nowrap" scope="row">{{ $number }}</th>
+                        <td class="px-4 py-2 text-sm font-normal text-left text-gray-500 whitespace-nowrap">{{ $event->nama_acara }}</td>
+                        <td class="px-4 py-2 text-sm font-normal text-left text-gray-500 whitespace-nowrap">{!! Str::limit($event->deskripsi_acara, $limit = 40, $end = '...') !!}</td>
+                        <td class="px-4 py-2 text-sm font-normal text-left text-gray-500 whitespace-nowrap">{{ $event->tanggal_acara }}</td>
+                        <td class="px-4 py-2 text-sm font-normal text-right whitespace-nowrap">
+                            <a href="/event/{{ $slug }}/{{ $event->id }}/edit" class="font-medium text-blue-600 transition duration-300 ease-in-out hover:text-blue-700 focus:text-blue-700 active:text-blue-800">Edit</a>
                             <form action="/event/{{ $slug }}/{{ $event->id }}" method="post" class="inline-block">
                                 @method('delete')
                                 @csrf
-                                <button type="submit" class="ml-2 font-medium text-red-600 hover:text-red-700 focus:text-red-700 active:text-red-800 transition duration-300 ease-in-out" onclick="return confirm('Are you sure?')">Delete
+                                <button type="submit" class="ml-2 font-medium text-red-600 transition duration-300 ease-in-out hover:text-red-700 focus:text-red-700 active:text-red-800" onclick="return confirm('Are you sure?')">Delete
                                 </button>
                             </form> 
                         </td>
@@ -57,8 +66,8 @@
                 </tbody>
             </table>
             @else
-            <div class="text-center py-5">
-                <p class="text-gray-500 mt-5 py-3">Event masih kosong</p>
+            <div class="py-5 text-center">
+                <p class="py-3 mt-5 text-gray-500">Event masih kosong</p>
             </div>
             @endif
         </div>
--- resources/views/admin/jurnal.blade.php
+++ resources/views/admin/jurnal.blade.php
@@ -3,49 +3,58 @@
 @section('container')
 <section class="ml-0 lg:ml-[250px] p-8">
     @if(session()->has('success'))
-        <div class="w-1/2 flex justify-between mx-auto bg-green-100 border-l-4 border-green-500 text-green-700 p-4 relative" role="alert">
+        <div id="success-message" class="relative flex justify-between w-1/2 p-4 mx-auto text-green-700 bg-green-100 border-l-4 border-green-500" role="alert">
             {{ session('success') }}
             <button
-                class=" text-green-600 hover:text-green-400 font-bold text-medium"
+                class="font-bold text-green-600 hover:text-green-400 text-medium"
                 onclick="this.parentElement.style.display='none'"
             >
                 X
             </button>
         </div>
+
+        <script>
+            setTimeout(() => {
+                var successMessage = document.getElementById("success-message");
+                if(successMessage){
+                    successMessage.style.display = 'none';
+                }
+            }, 3000);
+        </script>
     @endif
-    <h2 class="text-dark font-bold text-3xl mb-3">{{ $subtitle }}</h2>
-    <h2 class="text-dark font-small text-lg mb-10">{{ $title }}</h2>
-    <a href="/jurnal-admin/create" class="text-sm font-semibold text-white bg-primary py-3 px-6 rounded-md hover:shadow-lg hover:opacity-50 transition duration-300 ease-in-out">+ Tambah Jurnal</a>
-    <div class="rounded-lg shadow-lg bg-white flex flex-col overflow-x-auto mt-10 mb-10">
+    <h2 class="mb-3 text-3xl font-bold text-dark">{{ $subtitle }}</h2>
+    <h2 class="mb-10 text-lg text-dark font-small">{{ $title }}</h2>
+    <a href="/jurnal-admin/create" class="px-6 py-3 text-sm font-semibold text-white transition duration-300 ease-in-out rounded-md bg-primary hover:shadow-lg hover:opacity-50">+ Tambah Jurnal</a>
+    <div class="flex flex-col mt-10 mb-10 overflow-x-auto bg-white rounded-lg shadow-lg">
         <div class="inline-block min-w-full overflow-hidden">
             @if ($data->count())
             <table class="min-w-full mb-0">
-                <thead class="border-b rounded-t-lg text-left">
+                <thead class="text-left border-b rounded-t-lg">
                 <tr>
-                    <th scope="col" class="rounded-tl-lg text-sm font-medium px-4 py-2">No</th>
-                    <th scope="col" class="text-sm font-medium px-4 py-2">Gambar</th>
-                    <th scope="col" class="text-sm font-medium px-4 py-2">Judul</th>
-                    <th scope="col" class="text-sm font-medium px-4 py-2">Deskripsi</th>
-                    <th scope="col" class="text-sm font-medium px-4 py-2">Penulis</th>
-                    <th scope="col" class="text-sm font-medium px-4 py-2">Tanggal</th>
-                    <th scope="col" class="rounded-tr-lg text-sm font-medium px-4 py-2"></th>
+                    <th scope="col" class="px-4 py-2 text-sm font-medium rounded-tl-lg">No</th>
+                    <th scope="col" class="px-4 py-2 text-sm font-medium">Gambar</th>
+                    <th scope="col" class="px-4 py-2 text-sm font-medium">Judul</th>
+                    <th scope="col" class="px-4 py-2 text-sm font-medium">Deskripsi</th>
+                    <th scope="col" class="px-4 py-2 text-sm font-medium">Penulis</th>
+                    <th scope="col" class="px-4 py-2 text-sm font-medium">Tanggal</th>
+                    <th scope="col" class="px-4 py-2 text-sm font-medium rounded-tr-lg"></th>
                 </tr>
                 </thead>
                 <tbody>
                     @foreach ($data as $jurnal)   
-                    <tr class="border-b bg-gray-100">
-                        <th class="text-sm font-medium px-4 py-2 whitespace-nowrap text-left" scope="row">{{ $number }}</th>
+                    <tr class="bg-gray-100 border-b">
+                        <th class="px-4 py-2 text-sm font-medium text-left whitespace-nowrap" scope="row">{{ $number }}</th>
                         <td class="px-4 py-2 whitespace-nowrap"><img src="{{ asset('storage/' . $jurnal->image) }}" alt="{{ $jurnal->image }}"></td>
-                        <td class="px-4 py-2 whitespace-nowrap text-sm fomt-normal text-gray-500">{{ Str::limit($jurnal->title, $limit = 20, $end = '...') }}</td>
-                        <td class="text-sm font-normal px-4 py-2 whitespace-nowrap text-left text-gray-500">{!! Str::limit($jurnal->body, $limit = 30, $end = '...') !!}</td>
-                        <td class="text-sm font-normal px-4 py-2 whitespace-nowrap text-left text-gray-500">{{ Str::limit($jurnal->author, $limit = 15, $end = '...') }}</td>
-                        <td class="text-sm font-normal px-4 py-2 whitespace-nowrap text-left text-gray-500">{{  Str::limit($jurnal->created_at, $limit = 10, $end = '...') }}</td>
-                        <td class="text-sm font-normal px-4 py-2 whitespace-nowrap text-right">
-                            <a href="/jurnal-admin/{{ $jurnal->slug }}/edit" class="font-medium text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 transition duration-300 ease-in-out">Edit</a>
+                        <td class="px-4 py-2 text-sm text-gray-500 whitespace-nowrap fomt-normal">{{ Str::limit($jurnal->title, $limit = 20, $end = '...') }}</td>
+                        <td class="px-4 py-2 text-sm font-normal text-left text-gray-500 whitespace-nowrap">{!! Str::limit($jurnal->body, $limit = 30, $end = '...') !!}</td>
+                        <td class="px-4 py-2 text-sm font-normal text-left text-gray-500 whitespace-nowrap">{{ Str::limit($jurnal->author, $limit = 15, $end = '...') }}</td>
+                        <td class="px-4 py-2 text-sm font-normal text-left text-gray-500 whitespace-nowrap">{{  Str::limit($jurnal->created_at, $limit = 10, $end = '...') }}</td>
+                        <td class="px-4 py-2 text-sm font-normal text-right whitespace-nowrap">
+                            <a href="/jurnal-admin/{{ $jurnal->slug }}/edit" class="font-medium text-blue-600 transition duration-300 ease-in-out hover:text-blue-700 focus:text-blue-700 active:text-blue-800">Edit</a>
                             <form action="/jurnal-admin/{{ $jurnal->slug }}" method="post" class="inline-block">
                                 @method('delete')
                                 @csrf
-                                <button type="submit" class="ml-2 font-medium text-red-600 hover:text-red-700 focus:text-red-700 active:text-red-800 transition duration-300 ease-in-out" onclick="return confirm('Are you sure?')">Delete
+                                <button type="submit" class="ml-2 font-medium text-red-600 transition duration-300 ease-in-out hover:text-red-700 focus:text-red-700 active:text-red-800" onclick="return confirm('Are you sure?')">Delete
                                 </button>
                             </form>                            
                         </td>
@@ -55,7 +64,7 @@
                 </tbody>
             </table>
             @else
-                <p class="text-gray-500 text-center py-3">Jurnal masih kosong</p>
+                <p class="py-3 text-center text-gray-500">Jurnal masih kosong</p>
             @endif
         </div>
         {{ $data->links() }}
